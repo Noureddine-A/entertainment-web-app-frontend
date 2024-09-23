f857216282ef35168d33e0af9b0070f4f835ac0d
Noureddine-A <anoxybiosis@hotmail.com>
2024-09-23 14:14:38 +0200

    Added recommended section

--- src/components/home/HomePage.jsx
+++ src/components/home/HomePage.jsx
@@ -3,12 +3,14 @@ import React from "react";
 import "./HomePage.css";
 import Search from "./Search";
 import Trending from "./Trending";
+import Recommended from "./Recommended";
 
 const HomePage = () => {
   return (
     <div className="entertainment__home-container">
       <Search />
       <Trending />
+      <Recommended/>
     </div>
   );
 };
--- src/components/util/http.js
+++ src/components/util/http.js
@@ -5,3 +5,11 @@ export async function getTrendingContent() {
 
   return apiData.trending;
 }
+
+export async function getRecommendations() {
+  const response = await fetch("http://localhost:8080/entertainment/recommendation");
+
+  const apiData = await response.json();
+
+  return apiData.recommendedList;
+}
